10ada5a5f501d7a93de26d639f1e41d5c54f2e02
kolhizin <kolhizin@users.noreply.github.com>
2019-07-26 09:08:12 +0300

    Read test csv as floats

---
+++ proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/Classifier.java
@@ -0,0 +1,95 @@
+package com.kolhizin.testtflite;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import org.tensorflow.lite.Interpreter;
+
+public class Classifier {
+    private Interpreter tflite_;
+    private Activity activity_;
+
+    public Classifier(Activity activity) {
+        activity_ = activity;
+    }
+
+    private MappedByteBuffer loadModelFile(String modelPath) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity_.getAssets().openFd(modelPath);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    /*
+    // Writes Image data into a {@code ByteBuffer}.
+    private void convertBitmapToByteBuffer(Bitmap bitmap) {
+        if (imgData == null) {
+            return;
+        }
+        imgData.rewind();
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        // Convert the image to floating point.
+        int pixel = 0;
+        long startTime = SystemClock.uptimeMillis();
+        for (int i = 0; i < getImageSizeX(); ++i) {
+            for (int j = 0; j < getImageSizeY(); ++j) {
+                final int val = intValues[pixel++];
+                addPixelValue(val);
+            }
+        }
+        long endTime = SystemClock.uptimeMillis();
+        LOGGER.v("Timecost to put values into ByteBuffer: " + (endTime - startTime));
+    }
+
+    // Runs inference and returns the classification results.
+    public List<Recognition> recognizeImage(final Bitmap bitmap) {
+        // Log this method so that it can be analyzed with systrace.
+        Trace.beginSection("recognizeImage");
+
+        Trace.beginSection("preprocessBitmap");
+        convertBitmapToByteBuffer(bitmap);
+        Trace.endSection();
+
+        // Run the inference call.
+        Trace.beginSection("runInference");
+        long startTime = SystemClock.uptimeMillis();
+        runInference();
+        long endTime = SystemClock.uptimeMillis();
+        Trace.endSection();
+        LOGGER.v("Timecost to run model inference: " + (endTime - startTime));
+
+        // Find the best classifications.
+        PriorityQueue<Recognition> pq =
+                new PriorityQueue<Recognition>(
+                        3,
+                        new Comparator<Recognition>() {
+                            @Override
+                            public int compare(Recognition lhs, Recognition rhs) {
+                                // Intentionally reversed to put high confidence at the head of the queue.
+                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
+                            }
+                        });
+        for (int i = 0; i < labels.size(); ++i) {
+            pq.add(
+                    new Recognition(
+                            "" + i,
+                            labels.size() > i ? labels.get(i) : "unknown",
+                            getNormalizedProbability(i),
+                            null));
+        }
+        final ArrayList<Recognition> recognitions = new ArrayList<Recognition>();
+        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
+        for (int i = 0; i < recognitionsSize; ++i) {
+            recognitions.add(pq.poll());
+        }
+        Trace.endSection();
+        return recognitions;
+    }
+     */
+}
--- proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/MainActivity.java
+++ proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/MainActivity.java
@@ -3,39 +3,55 @@ package com.kolhizin.testtflite;
 import android.content.res.AssetFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import org.tensorflow.lite.Interpreter;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.nio.charset.Charset;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Interpreter tflite_;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try{
-            tflite_ = new Interpreter(loadModelFile("model.tflite"));
-            Toast.makeText(this, "Created TFLite.", Toast.LENGTH_SHORT);
-        }catch (IOException e){
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
-        }catch (Exception e){
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+        float [][] input = readTestCSV();
+    }
+
+    private float[][] readTestCSV() throws RuntimeException {
+        String line = "";
+        LinkedList<float[]> rawResult = new LinkedList<float[]>();
+        int prevSize = -1;
+        try {
+            InputStreamReader is = new InputStreamReader(getAssets().open("htr_test.csv"));
+            BufferedReader reader = new BufferedReader(is);
+
+            while ((line = reader.readLine()) != null) {
+                // Split the line into different tokens (using the comma as a separator).
+                String[] tokens = line.split(",");
+                if (prevSize >= 0 && (tokens.length != prevSize))
+                    throw new RuntimeException("Different number of numbers in lines!");
+                prevSize = tokens.length;
+                float [] res = new float[tokens.length];
+                for(int i = 0; i < tokens.length; i++)
+                    res[i] = Float.parseFloat(tokens[i]);
+                rawResult.add(res);
+            }
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error" + line, e);
+            e.printStackTrace();
         }
-
+        float [][] res = new float[rawResult.size()][];
+        int i = 0;
+        for (float [] v : rawResult)
+            res[i++] = v;
+        return res;
     }
 
-    private MappedByteBuffer loadModelFile(String modelPath) throws IOException {
-        AssetFileDescriptor fileDescriptor = getAssets().openFd(modelPath);
-        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
-        FileChannel fileChannel = inputStream.getChannel();
-        long startOffset = fileDescriptor.getStartOffset();
-        long declaredLength = fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
-    }
 }
