bbfa89afbf369f5f57aa59a99f7caded32a759fc
kolhizin <kolhizin@users.noreply.github.com>
2019-07-26 22:33:28 +0300

    Implemented inference

--- proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/Classifier.java
+++ proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/Classifier.java
@@ -2,9 +2,12 @@ package com.kolhizin.testtflite;
 
 import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
+import android.os.SystemClock;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import org.tensorflow.lite.Interpreter;
@@ -12,9 +15,22 @@ import org.tensorflow.lite.Interpreter;
 public class Classifier {
     private Interpreter tflite_;
     private Activity activity_;
+    private ByteBuffer inputData_ = null;
+    private float[][] outputData_ = null;
+    public final int batch_size = 1;
+    public final int input_rows = 128;
+    public final int input_cols = 32;
+    public final int output_rows = 32;
+    public final int output_cols = 102;
 
-    public Classifier(Activity activity) {
+
+    public Classifier(Activity activity, String modelPath) throws IOException {
         activity_ = activity;
+
+        tflite_ = new Interpreter(loadModelFile(modelPath));
+        inputData_ = ByteBuffer.allocateDirect(batch_size * input_rows * input_cols * 4);
+        inputData_.order(ByteOrder.nativeOrder());
+        outputData_ = new float[output_rows][output_cols];
     }
 
     private MappedByteBuffer loadModelFile(String modelPath) throws IOException {
@@ -26,44 +42,35 @@ public class Classifier {
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
     }
 
-    /*
-    // Writes Image data into a {@code ByteBuffer}.
-    private void convertBitmapToByteBuffer(Bitmap bitmap) {
-        if (imgData == null) {
-            return;
-        }
-        imgData.rewind();
-        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-        // Convert the image to floating point.
-        int pixel = 0;
-        long startTime = SystemClock.uptimeMillis();
-        for (int i = 0; i < getImageSizeX(); ++i) {
-            for (int j = 0; j < getImageSizeY(); ++j) {
-                final int val = intValues[pixel++];
-                addPixelValue(val);
+    private void setInput(float [][] input) throws RuntimeException{
+        if (inputData_ == null)
+            throw new RuntimeException("Internal input buffer is not initialized!");
+        if (input.length != input_rows)
+            throw new RuntimeException("Input row number does not match expected by model!");
+        if (input[0].length != input_cols)
+            throw new RuntimeException("Input col number does not match expected by model!");
+        inputData_.rewind();
+        for (int i = 0; i < input_rows; ++i) {
+            for (int j = 0; j < input_cols; ++j) {
+                inputData_.putFloat(input[i][j]);
             }
         }
-        long endTime = SystemClock.uptimeMillis();
-        LOGGER.v("Timecost to put values into ByteBuffer: " + (endTime - startTime));
     }
 
+    private void runInference(){
+        tflite_.run(inputData_, outputData_);
+    }
+
+    public float [][] detect(float [][] input){
+        setInput(input);
+        runInference();
+        return outputData_;
+    }
+
+    /*
     // Runs inference and returns the classification results.
     public List<Recognition> recognizeImage(final Bitmap bitmap) {
-        // Log this method so that it can be analyzed with systrace.
-        Trace.beginSection("recognizeImage");
-
-        Trace.beginSection("preprocessBitmap");
-        convertBitmapToByteBuffer(bitmap);
-        Trace.endSection();
-
         // Run the inference call.
-        Trace.beginSection("runInference");
-        long startTime = SystemClock.uptimeMillis();
-        runInference();
-        long endTime = SystemClock.uptimeMillis();
-        Trace.endSection();
-        LOGGER.v("Timecost to run model inference: " + (endTime - startTime));
-
         // Find the best classifications.
         PriorityQueue<Recognition> pq =
                 new PriorityQueue<Recognition>(
--- proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/MainActivity.java
+++ proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/MainActivity.java
@@ -14,22 +14,44 @@ import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.util.LinkedList;
 import java.util.List;
+import com.kolhizin.testtflite.Classifier;
 
 public class MainActivity extends AppCompatActivity {
+    private Classifier classifier_ = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        float [][] input = readTestCSV();
+
+        try {
+            AssetFileDescriptor fileDescriptor = this.getAssets().openFd("model.tflite");
+            classifier_ = new Classifier(this, "model.tflite");
+        }catch (IOException e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+        }
+
+        float [][] input = readAssetCSV("htr_test.csv");
+        float [][] output = null;
+        try {
+            output = classifier_.detect(input);
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+        }
+        if(output != null) {
+            int a = output.length;
+            int b = output[0].length;
+            int c = output[output.length - 1].length;
+        }
     }
 
-    private float[][] readTestCSV() throws RuntimeException {
+    private float[][] readAssetCSV(String fname) throws RuntimeException {
         String line = "";
         LinkedList<float[]> rawResult = new LinkedList<float[]>();
         int prevSize = -1;
         try {
-            InputStreamReader is = new InputStreamReader(getAssets().open("htr_test.csv"));
+            InputStreamReader is = new InputStreamReader(getAssets().open(fname));
             BufferedReader reader = new BufferedReader(is);
 
             while ((line = reader.readLine()) != null) {
