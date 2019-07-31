1df0cef80452aee1cbef36be3617c34956dffdb6
kolhizin <kolhizin@users.noreply.github.com>
2019-07-31 21:20:54 +0300

    Removed redundant code

--- proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/MainActivity.java
+++ proof-of-concepts/android-tests/test04_tflite/app/src/main/java/com/kolhizin/testtflite/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         try {
-            //Bitmap bmp = fitSizeBitmap(input_, 32, 128);
             float [][] in_tmp1 = transformGrayscale(input_);
             transformCutoff(in_tmp1, 0.7f, 1.0f, 0.0f);
             float [][] in_tmp2 = transformTrim(in_tmp1, 1e-3f);
@@ -85,25 +84,6 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 0; i < res0.size(); i++)
             res.append(chars.charAt(res0.get(i)));
         return res.toString();
-    }
-
-    private void saveImage(Bitmap finalBitmap, String image_name) {
-
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root);
-        myDir.mkdirs();
-        String fname = "Image-" + image_name+ ".jpg";
-        File file = new File(myDir, fname);
-        if (file.exists())
-            file.delete();
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     private Bitmap array2Bitmap(float [][] src){
@@ -293,36 +273,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return res;
     }
-
-    private float[][] readAssetCSV(String fname) throws RuntimeException {
-        String line = "";
-        LinkedList<float[]> rawResult = new LinkedList<float[]>();
-        int prevSize = -1;
-        try {
-            InputStreamReader is = new InputStreamReader(getAssets().open(fname));
-            BufferedReader reader = new BufferedReader(is);
-
-            while ((line = reader.readLine()) != null) {
-                // Split the line into different tokens (using the comma as a separator).
-                String[] tokens = line.split(",");
-                if (prevSize >= 0 && (tokens.length != prevSize))
-                    throw new RuntimeException("Different number of numbers in lines!");
-                prevSize = tokens.length;
-                float [] res = new float[tokens.length];
-                for(int i = 0; i < tokens.length; i++)
-                    res[i] = Float.parseFloat(tokens[i]);
-                rawResult.add(res);
-            }
-        } catch (IOException e) {
-            Log.e("MainActivity", "Error" + line, e);
-            e.printStackTrace();
-        }
-        float [][] res = new float[rawResult.size()][];
-        int i = 0;
-        for (float [] v : rawResult)
-            res[i++] = v;
-        return res;
-    }
-
 
 }
