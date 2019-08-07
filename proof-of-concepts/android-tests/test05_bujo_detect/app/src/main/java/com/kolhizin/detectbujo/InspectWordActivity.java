d54400a875cf0a822e9854ee85aee60778d18e12
kolhizin <kolhizin@users.noreply.github.com>
2019-08-07 07:15:32 +0300

    Added InspectWordActivity

---
+++ proof-of-concepts/android-tests/test05_bujo_detect/app/src/main/java/com/kolhizin/detectbujo/InspectWordActivity.java
@@ -0,0 +1,14 @@
+package com.kolhizin.detectbujo;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InspectWordActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inspect_word);
+    }
+}
